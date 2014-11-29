c5c5dd84728e35a6e5fbdab6a6ed9c1ec3adb19f
John <yehohanan7@gmail.com>
2014-11-29 23:08:19 +0000

    Inital commit

---
+++ init.el
@@ -0,0 +1,14 @@
+;; This sets up the load path so that we can override it
+(package-initialize nil)
+;; Override the packages with the git version of Org and other packages
+(add-to-list 'load-path "~/elisp/org-mode/lisp")
+(add-to-list 'load-path "~/elisp/org-mode/contrib/lisp")
+(add-to-list 'load-path "~/code/org2blog")
+(add-to-list 'load-path "~/Dropbox/2014/presentations/org-reveal")
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
+;; Load the rest of the packages
+(package-initialize t)
+(setq package-enable-at-startup nil)
+(require 'org)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "~/.emacs.d/john.org"))
