b13076a11b242d2e03a8ebc3b1f6fe906df12809
John Pradeep <yehohanan7@Johns-MacBook-Pro.local>
2015-06-17 18:58:31 +0200

    fixed keybindings

--- init.el
+++ init.el
@@ -8,4 +8,8 @@
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/john.org"))
+
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
 (put 'upcase-region 'disabled nil)
