c628671f063bd3e9db803c3ac009f22547c473f8
John <john.pradeep@klarna.com>
2015-11-05 22:00:10 +0100

    Configure swoop

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/john.org"))
 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-x\m" 'execute-extended-command)
+
 
 (put 'upcase-region 'disabled nil)
