1d129155953a9b59be33f0d42324320760092c56
John <yehohanan7@gmail.com>
2015-12-17 16:26:48 +0100

    Cleanup

--- custom/aws/aws.el
+++ custom/aws/aws.el
@@ -23,43 +23,31 @@
   (let* ((json-object-type 'hash-table))
     (json-read-from-string json)))
 
-(defun group-name (instance)
-  (let ((security-groups (gethash "SecurityGroups" instance)))
-    (when (> (length security-groups) 0)
-      (gethash "GroupName" (aref security-groups 0)))))
-
-(defun print-instance (instance)
-  (let ((id (gethash "InstanceId" instance))
-        (group (group-name instance))
-        (name (gethash "KeyName" instance))
-        (state (gethash "Name" (gethash "State" instance)))
-        (ip (gethash "PrivateIpAddress" instance))
-        (launch-time (gethash "LaunchTime" instance))
-        (type (gethash "InstanceType" instance)))
-    (print (s-join "   " (list id type name state ip group launch-time)))))
-
-(defun print-reservation (reservation)
-  (let ((instances (gethash "Instances" reservation)))
-    (mapc 'print-instance instances)))
-
 (defun print-output (region output)
-  (let ((reservations (gethash "Reservations" (json-to-hash output))))
-    (when (> (length reservations) 0) 
-      (print region)
-      (mapc 'print-reservation reservations))))
+  (let ((instances (json-to-hash output)))
+    (mapc 'print-instance instances)))
 
 (defun run-command (command region profile)
   "Runs the aws command & returns the output"
   (shell-command-to-string (format "aws %s --profile %s --region %s" command profile region)))
+
+(defun print-instance (instance)
+  (when (> (length instance) 0)
+    (let* ((instance (aref instance 0))
+           (id (gethash "id" instance))
+           (status (gethash "status" instance))
+           (ip (gethash "ip" instance))
+           (type (gethash "type" instance))
+           (group (gethash "group" instance)))
+      (print (s-join "   " (list id type status ip group))))))
+
 
 (defun ec2 (command region profile)
   (run-command (format "ec2 %s" command) region profile))
 
 (defun ec2-instances (profile instance-name)
   (interactive "sProfile:\nsName:")
-  (let ((command (format "describe-instances --filters \"Name=tag:Name, Values=%s\"" instance-name)))
+  (let ((command (format "describe-instances --filters \"Name=tag:Name, Values=%s\" --query \"Reservations[*].Instances[*].{id:InstanceId, status:State.Name, type:InstanceType, ip:PrivateIpAddress, group:SecurityGroups[0].GroupName}\"" instance-name)))
     (with-temp-buffer
       (dolist (region aws-regions)
         (print-output region (ec2 command region profile))))))
-
-
